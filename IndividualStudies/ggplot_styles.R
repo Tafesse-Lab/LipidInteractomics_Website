eb0c6e1d146f324d4b713c4f355f787cc0438411
Gaelen Guzman <gaelenguzman@gmail.com>
2025-01-16 11:27:31 -0800

    Updating individual study pages

--- IndividualStudies/ggplot_styles.R
+++ IndividualStudies/ggplot_styles.R
@@ -6,6 +6,80 @@ library(emojifont)
 library(dplyr)
 library(tidyr)
 
+# A standardized MA plot function
+
+MAStandard <- function(data) {
+
+    MAPlots <- data |>
+        ggplot(aes(x = AveExpr,
+                   y = logFC,
+                   color = factor(hit_annotation),
+                   fill = factor(hit_annotation),
+                   shape = factor(hit_annotation),
+                   alpha = factor(hit_annotation),
+                   size = factor(hit_annotation))) +
+        geom_point(aes(text = paste0("Gene name: ", gene_name, "\n",
+                                    "LogFC: ", logFC, "\n",
+                                    "p-value: ", pvalue, "\n"))) +
+        geom_hline(yintercept = 0, linetype = 2) +
+        geom_vline(xintercept = 0, linetype = 2) +
+        scale_x_continuous(limits=c(0.9 * min(abs(data$AveExpr)), 1.1* max(abs(data$AveExpr))))+
+        scale_y_continuous(limits=c(-1.1 * max(abs(data$logFC)), 1.1* max(abs(data$logFC))))+
+        scale_shape_manual(values = c("enriched hit" = 21,
+                                    "hit" = 21,
+                                    "enriched candidate" = 24,
+                                    "candidate" = 24,
+                                    "no hit" = 22), 
+                        name = "Trend") +
+        scale_color_manual(values = c("enriched hit" = "black",
+                                    "hit" = "black",
+                                    "enriched candidate" = "black",
+                                    "candidate" = "black",
+                                    "no hit" = "black"), 
+                        name = "Trend") +
+        scale_fill_manual(values = c("enriched hit" = "orange",
+                                    "hit" = "orange",
+                                    "enriched candidate" = "purple",
+                                    "candidate" = "purple",
+                                    "no hit" = "black"), 
+                        name = "Trend") +
+        scale_alpha_manual(values = c("enriched hit" = 0.8,
+                                    "hit" = 0.8,
+                                    "enriched candidate" = 0.5,
+                                    "candidate" = 0.5,
+                                    "no hit" = 0.25), 
+                        name = "Trend") +
+        scale_size_manual(values = c("enriched hit" = 4,
+                                    "hit" = 4,
+                                    "enriched candidate" = 2,
+                                    "candidate" = 2, 
+                                    "no hit" = 0.75), 
+                        name = "Trend") +
+        facet_wrap(~LipidProbe) +
+        ylab("Log2 fold-change") +
+        xlab("Average Ion Intensity") +
+        theme_minimal() +
+        theme(text = element_text(family = "serif")) +
+        theme(panel.border = element_rect(color = NA, fill = NA)) +
+        theme(legend.title = element_text(size = 12, face = "bold")) +
+        theme(axis.title = element_text(size = 12, face = "bold")) +
+        theme(legend.text = element_text(size = 12)) +
+        theme(strip.text = element_text(
+    size = 20))
+
+    m <- list(l=50, r = 50, b= 100, pad = 4)
+	
+    MAPlotly <- ggplotly(MAPlots,
+                            tooltip = c("text", dynamicTicks = TRUE),
+                            hovermode = "closest",
+                            autosize = T,
+                            margin = m) |>
+                    layout(showlegend = FALSE)
+    
+    return(MAPlotly)
+}
+
+
 # A standardized Ranked Order plot theme
 
 RankedOrderPlotStandard <- function(data) {
@@ -60,7 +134,9 @@ RankedOrderPlotStandard <- function(data) {
         theme(panel.border = element_rect(color = NA, fill = NA)) +
         theme(legend.title = element_text(size = 12, face = "bold")) +
         theme(axis.title = element_text(size = 12, face = "bold")) +
-        theme(legend.text = element_text(size = 12))
+        theme(legend.text = element_text(size = 12))  +
+        theme(strip.text = element_text(
+    size = 20))
 
     m <- list(l=50, r = 50, b= 100, pad = 4)
 	
@@ -131,7 +207,9 @@ VolcanoPlotStandardized <- function(data) {
         theme(strip.text = element_text(size = 12, face = "bold")) +
         theme(legend.title = element_text(size = 12, face = "bold")) +
         theme(axis.title = element_text(size = 12, face = "bold")) +
-        theme(legend.text = element_text(size = 12))
+        theme(legend.text = element_text(size = 12))  +
+        theme(strip.text = element_text(
+    size = 20))
 
     
     m <- list(l=50, r = 50, b= 100, pad = 10)
